daa75ad0f9ad751efa674385a76970723a2f5b51
lcfme <lcfme@sina.com>
2018-10-23 10:39:38 +0800

    asdf

--- pages/gameend/gameend.js
+++ pages/gameend/gameend.js
@@ -84,14 +84,13 @@ Page(connect()({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-    this.fetchUserData();
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    this.fetchUserData();
   },
 
   /**
